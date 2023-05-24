8afb527c8845897b1ca7552753936dc06e2c4b54
Pampaj <leonardo.pamaploni666@gmail.com>
2023-05-24 14:11:53 +0200

    Syncronizing

--- BuildBipartiteGraph.py
+++ BuildBipartiteGraph.py
@@ -34,7 +34,6 @@ def build_bipartite_graph(dataset):
                 'venue': row['journal']
             }
             authors = authors.split('|')
-
             publication_id = row['id']
         elif isinstance(authors, float) and math.isnan(authors):
             continue
